aab957c874c91d2bd6c8dd432267770c90aaf59c
Cauani Castro <cauani.castro@hotmail.com>
2019-12-29 14:11:22 -0700

    create script to handle enemies and their behaviours

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float lifeMax = 5f;
+    float currentLife;
+    public List<Firearm> vulnerableTo;
+    List<string> vulnerableToNames;
+
+    private void Start()
+    {
+        this.currentLife = this.lifeMax;
+        this.vulnerableToNames = (from weapon in vulnerableTo select weapon.bulletTrigger).ToList();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet" && vulnerableToNames.Contains(collision.gameObject.name))
+        {
+            Debug.Log("hittou");
+            //TODO: reduce life, add dying animation, those things
+            Destroy(gameObject, 0.15f);
+        }
+    }
+
+
+}
